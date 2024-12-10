e6dba5962cc283d8855f62cb8bd80c86fd48c4e1
graham <scocker@wgu.edu>
2024-12-10 18:27:40 -0500

    B2: modified the front end to display reservation price in currency rates USD, EUR, and CAD on different lines

--- src/main/UI/src/app/app.component.ts
+++ src/main/UI/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
-import {HttpClient, HttpResponse,HttpHeaders} from "@angular/common/http";
-import { Observable } from 'rxjs';
+import {HttpClient, HttpResponse, HttpHeaders} from "@angular/common/http";
+import {Observable} from 'rxjs';
 import {map} from "rxjs/operators";
 
 @Component({
@@ -9,37 +9,38 @@ import {map} from "rxjs/operators";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   welcomeMessageEng$!: Observable<string>
   welcomeMessageFre$!: Observable<string>
 
-  constructor(private httpClient:HttpClient){}
+  constructor(private httpClient: HttpClient) {
+  }
 
-  private baseURL:string='http://localhost:8080';
+  private baseURL: string = 'http://localhost:8080';
 
-  private getUrl:string = this.baseURL + '/room/reservation/v1/';
-  private postUrl:string = this.baseURL + '/room/reservation/v1';
-  public submitted!:boolean;
-  roomsearch! : FormGroup;
-  rooms! : Room[];
-  request!:ReserveRoomRequest;
-  currentCheckInVal!:string;
-  currentCheckOutVal!:string;
+  private getUrl: string = this.baseURL + '/room/reservation/v1/';
+  private postUrl: string = this.baseURL + '/room/reservation/v1';
+  public submitted!: boolean;
+  roomsearch!: FormGroup;
+  rooms!: Room[];
+  request!: ReserveRoomRequest;
+  currentCheckInVal!: string;
+  currentCheckOutVal!: string;
 
-    ngOnInit(){
-      // english welcome message
-      this.welcomeMessageEng$ = this.httpClient.get(this.baseURL + "/welcome/?lang=en-US", {responseType: "text"})
+  ngOnInit() {
+    // english welcome message
+    this.welcomeMessageEng$ = this.httpClient.get(this.baseURL + "/welcome/?lang=en-US", {responseType: "text"})
 
-      // french welcome message
-      this.welcomeMessageFre$ = this.httpClient.get(this.baseURL + "/welcome/?lang=fr-CA", {responseType: "text"})
+    // french welcome message
+    this.welcomeMessageFre$ = this.httpClient.get(this.baseURL + "/welcome/?lang=fr-CA", {responseType: "text"})
 
-      this.roomsearch= new FormGroup({
-        checkin: new FormControl(' '),
-        checkout: new FormControl(' ')
-      });
+    this.roomsearch = new FormGroup({
+      checkin: new FormControl(' '),
+      checkout: new FormControl(' ')
+    });
 
- //     this.rooms=ROOMS;
+    //     this.rooms=ROOMS;
 
 
     const roomsearchValueChanges$ = this.roomsearch.valueChanges;
@@ -51,70 +52,77 @@ export class AppComponent implements OnInit{
     });
   }
 
-    onSubmit({value,valid}:{value:Roomsearch,valid:boolean}){
-      this.getAll().subscribe(
+  onSubmit({value, valid}: { value: Roomsearch, valid: boolean }) {
+    // @ts-ignore
+    this.getAll().subscribe(
+      rooms => {
+        console.log(Object.values(rooms)[0]);
+        this.rooms = <Room[]>Object.values(rooms)[0];
+        this.rooms.forEach(room => {
+          room.euroPrice = room.price;
+          room.cadPrice = room.price;
+        })
+      }
+    );
+  }
 
-        rooms => {console.log(Object.values(rooms)[0]);this.rooms=<Room[]>Object.values(rooms)[0]; }
+  reserveRoom(value: string) {
+    this.request = new ReserveRoomRequest(value, this.currentCheckInVal, this.currentCheckOutVal);
 
+    this.createReservation(this.request);
+  }
 
-      );
-    }
-    reserveRoom(value:string){
-      this.request = new ReserveRoomRequest(value, this.currentCheckInVal, this.currentCheckOutVal);
+  createReservation(body: ReserveRoomRequest) {
+    let bodyString = JSON.stringify(body); // Stringify payload
+    let headers = new Headers({'Content-Type': 'application/json'}); // ... Set content type to JSON
+    // let options = new RequestOptions({headers: headers}); // Create a request option
 
-      this.createReservation(this.request);
-    }
-    createReservation(body:ReserveRoomRequest) {
-      let bodyString = JSON.stringify(body); // Stringify payload
-      let headers = new Headers({'Content-Type': 'application/json'}); // ... Set content type to JSON
-     // let options = new RequestOptions({headers: headers}); // Create a request option
-
-     const options = {
+    const options = {
       headers: new HttpHeaders().append('key', 'value'),
 
     }
 
-      this.httpClient.post(this.postUrl, body, options)
-        .subscribe(res => console.log(res));
-    }
+    this.httpClient.post(this.postUrl, body, options)
+      .subscribe(res => console.log(res));
+  }
 
   /*mapRoom(response:HttpResponse<any>): Room[]{
     return response.body;
   }*/
 
-    getAll(): Observable<any> {
+  getAll(): Observable<any> {
 
 
-       return this.httpClient.get(this.baseURL + '/room/reservation/v1?checkin='+ this.currentCheckInVal + '&checkout='+this.currentCheckOutVal, {responseType: 'json'});
-    }
-
+    return this.httpClient.get(this.baseURL + '/room/reservation/v1?checkin=' + this.currentCheckInVal + '&checkout=' + this.currentCheckOutVal, {responseType: 'json'});
   }
-
-
-
-export interface Roomsearch{
-    checkin:string;
-    checkout:string;
-  }
-
-
-
-
-export interface Room{
-  id:string;
-  roomNumber:string;
-  price:string;
-  links:string;
 
 }
-export class ReserveRoomRequest {
-  roomId:string;
-  checkin:string;
-  checkout:string;
 
-  constructor(roomId:string,
-              checkin:string,
-              checkout:string) {
+
+export interface Roomsearch {
+  checkin: string;
+  checkout: string;
+}
+
+
+export interface Room {
+  cadPrice: string;
+  euroPrice: string;
+  id: string;
+  links: string;
+  price: string;
+  roomNumber: string;
+
+}
+
+export class ReserveRoomRequest {
+  roomId: string;
+  checkin: string;
+  checkout: string;
+
+  constructor(roomId: string,
+              checkin: string,
+              checkout: string) {
 
     this.roomId = roomId;
     this.checkin = checkin;
